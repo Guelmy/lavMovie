7cc374a614116377e0ca6f158d3f9fde928405f0
Guelmy Perez <pguelmi@gmail.com>
2019-07-10 21:54:00 -0400

    created model movie and favorite_movie

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->integer('age');
-            $table->string('email')->unique();
+            $table->string('email',8)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
